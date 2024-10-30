3805a680d514b60f4613d9258202962bc39cb95b
gabeman <gabrielayochukwu@gmail.com>
2024-10-30 04:42:19 +0100

    created jwtProvider

---
+++ food-app/food-app/src/main/java/com/example/food_app/Configuration/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.example.food_app.Configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class JwtProvider {
+    private SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    // method to generate Token
+    public String generateToken(Authentication auth){
+        // collect roles for authentication
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles = populateAuthorities(authorities);
+
+        String jwt = Jwts.builder()
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime()+86400000))
+                .claim("email",auth.getName())
+                .claim("authorities",roles)
+                .signWith(key)
+                .compact();
+        return jwt;
+    }
+
+    // get email from jwt token
+    public String getEmailFromJwtToken (String jwt){
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+
+        String email = String.valueOf(claims.get("email"));
+        return email;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auths = new HashSet<>();
+
+        for (GrantedAuthority authority : authorities){
+            auths.add(authority.getAuthority());
+        }
+        return String.join(",",auths);
+    }
+}
