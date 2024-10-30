d7b08771879dc86bdc5bf5ff3c579b9a7e3da33b
gabeman <gabrielayochukwu@gmail.com>
2024-10-30 04:25:10 +0100

    created CustomerUserDetailsService to consume spring security generated password

--- food-app/food-app/src/main/java/com/example/food_app/Service/CustomerUserDetailsService.java
+++ food-app/food-app/src/main/java/com/example/food_app/Service/CustomerUserDetailsService.java
@@ -1,14 +1,38 @@
 package com.example.food_app.Service;
 
-import org.springframework.security.core.userdetails.User;
+import com.example.food_app.enums.USER_ROLE;
+import com.example.food_app.model.User;
+import com.example.food_app.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerUserDetailsService implements UserDetailsService {
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user =
-        return null;
+        User user = userRepository.findByEmail(username);
+                if(user == null){
+                    throw new UsernameNotFoundException("user not found with email"+username);
+                }
+
+                // we get user and its role
+        USER_ROLE role = user.getRole();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+                // add the roles
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),authorities);
     }
 }
